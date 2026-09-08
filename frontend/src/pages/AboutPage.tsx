b82024ca2756cb2a9d3b387bc57b5d3f09541bdf
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2026-09-08 17:43:21 +0300

    improved the about page

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,3 +1,24 @@
+import "../HomePage.css";
+import backgroundImageHoney from "../assets/backgroundImageHoney.jpg";
+
 export default function AboutPage() {
-  return <h1>default placeholder for the about page...</h1>;
+  return (
+    <div
+      className="home-container"
+      style={{
+        backgroundImage: `url(${backgroundImageHoney})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div className="about-content">
+        <h1 className="about-message">TEMPLATE DE MESAJ PENTRU ABOUT</h1>
+        <p className="about-description">
+          DADASDASDASDASDASDASDASDASDASDASDASD
+        </p>
+        <p className="about-signature">– Bogdan Miere</p>
+      </div>
+    </div>
+  );
 }
