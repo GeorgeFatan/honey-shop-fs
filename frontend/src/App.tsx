72e9fb9ab14981aa642ced8530b1c813e7d27cdb
Claudiu6789 <127293765+Claudiu6789@users.noreply.github.com>
2026-09-12 18:04:54 +0300

    ContactUs page
    Contact Us page is added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import Orar from "./pages/Orar";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/orar" element={<Orar />} />
+      <Route path="/contact" element={<ContactUs />} />
     </Routes>
   );
 }
