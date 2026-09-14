054d38600be2113dff73024019045667299efe0a
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2026-09-14 15:27:33 +0300

    Fixed UI and added new background image for the app

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
 import "../HomePage.css";
-import backgroundImageHoney from "../assets/backgroundImageHoney.jpg";
+import backgroundImageHoney from "../assets/backgroundImageBuna.png";
 //import { Link } from "react-router-dom"; // 1. Importăm Link care ne ajuta sa adaugam butoane pe pagina.
+import miereSalcam from "../assets/miereSalcam.png";
+import miereTei from "../assets/miereTei.png";
+import mierePoliFlora from "../assets/mierePoliFlora.png";
 
 export default function HomePage() {
   return (
@@ -22,9 +25,9 @@ export default function HomePage() {
           Miere naturală, direct din stupii noștri, produsă cu grijă în inima
           Sibiului.
         </p>
-        
+
         {/* 2. Adăugăm link-ul către pagina de orar */}
-       {/* {/* 2. Adăugăm link-ul către pagina de orar 
+        {/* {/* 2. Adăugăm link-ul către pagina de orar 
         <Link to="/orar" className="orar-link-button">
           Vezi Orarul de Funcționare
         </Link>*/}
@@ -36,19 +39,19 @@ export default function HomePage() {
 
         <div className="product-grid">
           <div className="product-card">
-            <img src="https://via.placeholder.com/150" alt="Honey Jar" />
+            <img src={mierePoliFlora} alt="Honey Jar" />
             <h3>Miere Polifloră</h3>
             <p>Miere bogată și aromată, obținută din flori sălbatice.</p>
           </div>
 
           <div className="product-card">
-            <img src="https://via.placeholder.com/150" alt="Honey Jar" />
+            <img src={miereSalcam} alt="Honey Jar" />
             <h3>Miere de Salcâm</h3>
             <p>Miere delicată, cu note florale fine.</p>
           </div>
 
           <div className="product-card">
-            <img src="https://via.placeholder.com/150" alt="Honey Jar" />
+            <img src={miereTei} alt="Honey Jar" />
             <h3>Miere de Tei</h3>
             <p>Miere dulce și catifelată, din flori de tei.</p>
           </div>
