1ac35be889a4496081407cebf6ba923eb2c88649
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2026-09-15 13:57:59 +0300

    I added the background css style on #root
    #Root nu se re-randeaza. => backgroundu ramane cat de cat fluid cu paginile cand le incarcam.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
+    <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
